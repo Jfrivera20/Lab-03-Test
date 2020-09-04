a81bd9aa46e887399996107d84bed441316b77f6
Justin <justin.rivera2@upr.edu>
2020-09-03 23:08:04 -0400

    init

---
+++ Cow.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+void Cow :: setWeight(int w){
+    weight = w;
+    }
+void Cow :: setHeight(int h){
+        height = h; 
+    }
+void Cow :: setColor(string black){
+        color = black;
+    }
+int Cow :: getWeight(){
+        return weight;
+    }
+int Cow :: getHeight(){
+        return height;
+    }
+int Cow :: getColor(){
+        return color;
+    }
+}
---
+++ Cow.h
@@ -0,0 +1,24 @@
+#include <string.h>
+class Cow{
+
+    private:
+    int weight;
+    int height;
+    string color;
+
+    public:
+    void setWeight(int w);
+    void setHeight(int h);
+    void setColor(string black);
+    int getWeight();
+    int getHeight();
+    string getColor();
+    
+};
+
+
+
+
+
+
+
